d8f90b4a8b93e992bb0f5da633e2abd8f35b766d
ilyakharlamov2023 <123149398+ilyakharlamov2023@users.noreply.github.com>
2023-01-20 10:59:45 +0300

    Add files via upload
    Signed-off-by: ilyakharlamov2023 <123149398+ilyakharlamov2023@users.noreply.github.com>

---
+++ temp/footer.php
@@ -0,0 +1,12 @@
+<footer style="background-color:  cadetblue;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <p style="text-align: center; color: blue; text-shadow: 1px 1px white; padding-top: 1%;">&copy; Copyright | INTEL - DEAF 2023г</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
